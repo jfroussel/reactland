e0c632e6a4b08ede2e072bfe831cd069fff2e99f
jeff <jfroussel75@gmail.com>
2020-03-27 19:35:02 +0100

    add article page

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -17,4 +17,11 @@ class BlogController extends AbstractController
         ]);
     }
 
+     /**
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function show() {
+        return $this->render('blog/show.html.twig');
+    }
+
 }
